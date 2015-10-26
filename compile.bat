1f85c98f0a2b25e4d519c248508fbd80530b933c
Lennie De Villiers <lennie.work@gmail.com>
2015-10-26 14:38:33 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
 C:\Users\lennie\Documents\Projects\CSharpAsmjs\il2c\Il2c.exe /roslyn /android /emscripten C:\Users\lennie\Documents\Projects\CSharpAsmjs\HelloWorld\HelloWorld\HelloWorld.cs /corelib:CoreLib.dll
 
 C:\Users\lennie\Documents\Projects\CSharpAsmjs\il2c\Il2c.exe /roslyn /android /emscripten CoreLib.dll
+
+emcc HelloWorld.cpp -o Hello.html
